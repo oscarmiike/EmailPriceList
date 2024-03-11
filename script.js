8b951a1eb9663039f03babef47a95e50f68ce22f
Shaun Hay <38158748+oscarmiike@users.noreply.github.com>
2024-03-11 10:41:14 +1000

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // global variables for price calcs
 let audPrice = 0, audOldPrice = 0, usdPrice = 0, usdOldPrice = 0, xauSpotAU = 0, xagSpotAU = 0, ausSpotAU = 0, agsSpotAU = 0, btcSpotAU = 0, ethSpotAU = 0, xauSpotUS = 0, xagSpotUS = 0, ausSpotUS = 0, agsSpotUS = 0, btcSpotUS = 0, ethSpotUS = 0, xauOldSpotAU = 0, xagOldSpotAU = 0, ausOldSpotAU = 0, agsOldSpotAU = 0, btcOldSpotAU = 0, ethOldSpotAU = 0, xauOldSpotUS = 0, xagOldSpotUS = 0, ausOldSpotUS = 0, agsOldSpotUS = 0, btcOldSpotUS = 0, ethOldSpotUS = 0;
-let globalToken = '';
 const token = getCookie('apiToken');
+const tokenTemp = '';
 const inputGroup = document.querySelector('.input-group');
 const fetchContainer = document.querySelector('.fetch-container');
 const helpContainer = document.querySelector('.helpContainer');
@@ -115,7 +115,7 @@ function saveToken() {
     }
 
     document.cookie = `apiToken=${token};path=/`;
-    globalToken = token;
+    tokenTemp = token;
 
     fadeOut(inputGroup, () => {
         tokenInput.value = '';
@@ -142,7 +142,6 @@ function delay(ms) {
 
 
 function fetchCombinedData() {
-    const token = globalToken;
     const loader = document.querySelector('.lds-grid');
     const refreshedMessage = document.getElementById('refreshedMessage'); 
 
